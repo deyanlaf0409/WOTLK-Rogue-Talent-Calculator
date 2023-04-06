0c0726866138b9a0a81d053fbc003bd7bd989fff
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-06 22:04:15 +0300

    fix stuff

--- Src/points_count.py
+++ Src/points_count.py
@@ -7,7 +7,7 @@ total_points = 0
 def update_counter(button, available_points_label, row):
     global total_points, AVAILABLE_POINTS
     # print(row)
-    row_available_points = row * 5
+    row_required_points = row * 5
 
     # Get the current counter value from the counter label widget
     count = int(button.counter["text"].split('/')[0])
@@ -15,7 +15,7 @@ def update_counter(button, available_points_label, row):
 
     # If there are available points and the row points are less than or equal to the available points,
     # and the total points is greater than or equal to the required points for the current row, increment the counter
-    if AVAILABLE_POINTS > 0 and total_points >= row_available_points and count < max_count:
+    if AVAILABLE_POINTS > 0 and total_points >= row_required_points and count < max_count:
         total_points += 1
         AVAILABLE_POINTS -= 1
         count += 1
