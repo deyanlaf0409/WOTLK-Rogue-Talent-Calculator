ef34eabed027e4f73de3e4ab113c697d5400251f
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-06 15:49:25 +0300

    Initial commit

---
+++ Subtlety/gui.py
@@ -0,0 +1,68 @@
+from tkinter import *
+import tkinter as tk
+from PIL import ImageTk, Image #test
+ROWS = 11
+COLS = 4
+AVAILABLE_POINTS = 80
+
+# Define a function to update the button counter
+def update_counter(button):
+    global total_points, AVAILABLE_POINTS, available_points_label
+
+    # Get the current counter value from the counter label widget
+    count = int(button.counter["text"])
+
+    # If total points exceed the maximum, prevent further increment
+    if total_points >= 80:
+        return
+
+    # Increment the counter value
+    count += 1
+
+    # Update the counter label widget to show the new counter value
+    button.counter["text"] = str(count)
+
+    # Update the total points
+    total_points += 1
+    AVAILABLE_POINTS -= 1
+
+    available_points_label.config(text=f"Available Points: {AVAILABLE_POINTS}")
+
+
+# Create a window
+window = tk.Tk()
+window.title("Button Grid")
+
+# Define the images for each button
+image6 = ImageTk.PhotoImage(Image.open("Images/ability_stealth.jpg"))
+
+# Create a grid of buttons
+total_points = 0
+for row in range(ROWS):
+    for col in range(COLS):
+        # Create a frame to hold the button and counter
+        frame = tk.Frame(window, width=50, height=50)
+        frame.grid(row=row, column=col, padx=5, pady=5)
+
+        # Create the button
+        if row == 1 and col == 2:
+            button = tk.Button(frame, image=image6, width=50, height=50)
+        else:
+            button = tk.Button(frame, width=6, height=3)
+
+        button.pack(side=tk.LEFT, padx=5)
+        button.config(command=lambda btn=button: update_counter(btn))
+
+        # Create the counter label
+        counter = tk.Label(frame, text="0", width=2, height=1, bg="gray", fg="white")
+        counter.pack(side=tk.RIGHT, anchor="se")
+
+        # Associate the counter label widget with the button widget
+        button.counter = counter
+
+# Create a label for displaying the available points
+available_points_label = tk.Label(window, text=f"Available Points: {AVAILABLE_POINTS}")
+available_points_label.grid(row=ROWS+1, column=0, columnspan=COLS)
+
+# Start the main event loop
+window.mainloop()
