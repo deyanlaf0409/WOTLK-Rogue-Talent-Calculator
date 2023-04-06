765ad8522d1ef1f26fc0226a87feb4b6198b1e8d
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-06 21:44:04 +0300

    Finaly! a tier logic and more images

--- Src/points_count.py
+++ Src/points_count.py
@@ -1,31 +1,29 @@
-
 ROWS = 11
 COLS = 4
 AVAILABLE_POINTS = 80
 total_points = 0
-def update_counter(button, available_points_label):
+
+
+def update_counter(button, available_points_label, row):
     global total_points, AVAILABLE_POINTS
+    # print(row)
+    row_available_points = row * 5
 
     # Get the current counter value from the counter label widget
     count = int(button.counter["text"].split('/')[0])
     max_count = int(button.counter["text"].split('/')[1])
 
-    # If total points exceed the maximum, prevent further increment
-    if total_points >= AVAILABLE_POINTS:
-        return
-
-    # Increment the counter value
-    if count < max_count:
+    # If there are available points and the row points are less than or equal to the available points,
+    # and the total points is greater than or equal to the required points for the current row, increment the counter
+    if AVAILABLE_POINTS > 0 and total_points >= row_available_points and count < max_count:
+        total_points += 1
+        AVAILABLE_POINTS -= 1
         count += 1
+    elif count == max_count:
+        button['state'] = 'disable'
     else:
         return
 
     # Update the counter label widget to show the new counter value
     button.counter["text"] = f"{count}/{max_count}"
-
-    # Update the total points
-    total_points += 1
-    AVAILABLE_POINTS -= 1
-
     available_points_label.config(text=f"Available Points: {AVAILABLE_POINTS}")
-
--- Subtlety/subtlety_talents_interface.py
+++ Subtlety/subtlety_talents_interface.py
@@ -1,4 +1,3 @@
-from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image
 from Src.points_count import *
@@ -13,15 +12,21 @@ window['background'] = background_clr
 
 # Create a label for displaying the available points
 available_points_label = tk.Label(window, text=f"Available Points: {AVAILABLE_POINTS}")
-available_points_label.grid(row=ROWS + 1, column=0, columnspan=COLS)
+available_points_label.grid(row=ROWS, column=0, columnspan=COLS)
 
 # Define the images for each button
-image1 = ImageTk.PhotoImage(Image.open("Images/ability_warrior_decisivestrike.jpg"))
-image2 = ImageTk.PhotoImage(Image.open("Images/spell_shadow_charm.jpg"))
-image3 = ImageTk.PhotoImage(Image.open("Images/ability_warrior_warcry.jpg"))
-image4 = ImageTk.PhotoImage(Image.open("Images/ability_rogue_feint.jpg"))
-image5 = ImageTk.PhotoImage(Image.open("Images/ability_sap.jpg"))
-image6 = ImageTk.PhotoImage(Image.open("Images/ability_stealth.jpg"))
+image_1 = ImageTk.PhotoImage(Image.open("Images/ability_warrior_decisivestrike.jpg"))
+image_2 = ImageTk.PhotoImage(Image.open("Images/spell_shadow_charm.jpg"))
+image_3 = ImageTk.PhotoImage(Image.open("Images/ability_warrior_warcry.jpg"))
+image_4 = ImageTk.PhotoImage(Image.open("Images/ability_rogue_feint.jpg"))
+image_5 = ImageTk.PhotoImage(Image.open("Images/ability_sap.jpg"))
+image_6 = ImageTk.PhotoImage(Image.open("Images/ability_stealth.jpg"))
+image_7 = ImageTk.PhotoImage(Image.open("Images/spell_magic_lesserinvisibilty.jpg"))
+image_8 = ImageTk.PhotoImage(Image.open("Images/spell_shadow_curse.jpg"))
+image_9 = ImageTk.PhotoImage(Image.open("Images/inv_sword_17.jpg"))
+image_10 = ImageTk.PhotoImage(Image.open("Images/spell_nature_mirrorimage.jpg"))
+image_11 = ImageTk.PhotoImage(Image.open("Images/spell_shadow_fumble.jpg"))
+image_12 = ImageTk.PhotoImage(Image.open("Images/ability_rogue_ambush.jpg"))
 
 # Create a grid of buttons
 total_points = 0
@@ -31,45 +36,74 @@ for row in range(ROWS):
         frame = tk.Frame(window, width=50, height=50, bg=background_clr)
         frame.grid(row=row, column=col, padx=5, pady=5)
 
+
         # Create the button
         if row == 0 and col == 0:
-            button = tk.Button(frame, image=image1, width=50, height=50, bg=button_clr)
+            button = tk.Button(frame, image=image_1, width=50, height=50, bg=button_clr)
             counter = tk.Label(frame, text="0/5", width=2, height=1, bg="gray", fg="white")
             counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
         elif row == 0 and col == 1:
-            button = tk.Button(frame, image=image2, width=50, height=50, bg=button_clr)
+            button = tk.Button(frame, image=image_2, width=50, height=50, bg=button_clr)
             counter = tk.Label(frame, text="0/3", width=2, height=1, bg="gray", fg="white")
             counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
         elif row == 0 and col == 2:
-            button = tk.Button(frame, image=image3, width=50, height=50, bg=button_clr)
+            button = tk.Button(frame, image=image_3, width=50, height=50, bg=button_clr)
             counter = tk.Label(frame, text="0/2", width=2, height=1, bg="gray", fg="white")
             counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
         elif row == 1 and col == 0:
-            button = tk.Button(frame, image=image4, width=50, height=50, bg=button_clr)
+            button = tk.Button(frame, image=image_4, width=50, height=50, bg=button_clr)
             counter = tk.Label(frame, text="0/2", width=2, height=1, bg="gray", fg="white")
             counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
         elif row == 1 and col == 1:
-            button = tk.Button(frame, image=image5, width=50, height=50, bg=button_clr)
+            button = tk.Button(frame, image=image_5, width=50, height=50, bg=button_clr)
             counter = tk.Label(frame, text="0/2", width=2, height=1, bg="gray", fg="white")
             counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
         elif row == 1 and col == 2:
-            button = tk.Button(frame, image=image6, width=50, height=50, bg=button_clr)
+            button = tk.Button(frame, image=image_6, width=50, height=50, bg=button_clr)
             counter = tk.Label(frame, text="0/3", width=2, height=1, bg="gray", fg="white")
             counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
+        elif row == 2 and col == 0:
+            button = tk.Button(frame, image=image_7, width=50, height=50, bg=button_clr)
+            counter = tk.Label(frame, text="0/2", width=2, height=1, bg="gray", fg="white")
+            counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
+        elif row == 2 and col == 1:
+            button = tk.Button(frame, image=image_8, width=50, height=50, bg=button_clr)
+            counter = tk.Label(frame, text="0/1", width=2, height=1, bg="gray", fg="white")
+            counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
+        elif row == 2 and col == 2:
+            button = tk.Button(frame, image=image_9, width=50, height=50, bg=button_clr)
+            counter = tk.Label(frame, text="0/3", width=2, height=1, bg="gray", fg="white")
+            counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
+        elif row == 3 and col == 0:
+            button = tk.Button(frame, image=image_10, width=50, height=50, bg=button_clr)
+            counter = tk.Label(frame, text="0/3", width=2, height=1, bg="gray", fg="white")
+            counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
+        elif row == 3 and col == 1:
+            button = tk.Button(frame, image=image_11, width=50, height=50, bg=button_clr)
+            counter = tk.Label(frame, text="0/3", width=2, height=1, bg="gray", fg="white")
+            counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
+        elif row == 3 and col == 2:
+            button = tk.Button(frame, image=image_12, width=50, height=50, bg=button_clr)
+            counter = tk.Label(frame, text="0/2", width=2, height=1, bg="gray", fg="white")
+            counter.pack(side=tk.RIGHT, anchor="se")
+            button.counter = counter
         else:
             button = tk.Button(frame, width=6, height=3, bg=button_clr)
 
         button.pack(side=tk.LEFT)
-        button.config(command=lambda btn=button: update_counter(btn, available_points_label))
+        button.config(command=lambda row=row, btn=button: update_counter(btn, available_points_label, row))
 
-        # Create the counter label
-
-        # Associate the counter label widget with the button widget
-        button.counter = counter
-
-    # Create a label for displaying the available points
-    #available_points_label = tk.Label(window, text=f"Available Points: {AVAILABLE_POINTS}")
-    #available_points_label.grid(row=ROWS + 1, column=0, columnspan=COLS)
 
 # Start the main event loop
 window.mainloop()
